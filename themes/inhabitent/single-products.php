8fa9b4f1d6e872dcdc6dc4e690e23a9a2712f61b
cjrt27 <cameron.jr.thorn@gmail.com>
2018-06-05 18:15:34 +0100

    removing comments section from products page

--- themes/inhabitent/single-products.php
+++ themes/inhabitent/single-products.php
@@ -44,18 +44,6 @@ get_header(); ?>
 			</article><!-- #post-## -->
 			<footer class="entry-footer">
 				</footer><!-- .entry-footer -->
-
-		
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
