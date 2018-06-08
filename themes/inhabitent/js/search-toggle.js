c870662c65972ef6aae19d91607636f7280cd4d1
cjrt27 <cameron.jr.thorn@gmail.com>
2018-06-08 16:28:30 +0100

    fixing 404 page

--- themes/inhabitent/js/search-toggle.js
+++ themes/inhabitent/js/search-toggle.js
@@ -3,32 +3,32 @@
 (function ($) {
   'use strict';
 
-  $( '.search-submit' ).click(function( event ) {
+  $( '#site-navigation .search-submit' ).click(function( event ) {
     event.preventDefault();
   });
 
-  $('.search-field').hide();
+  $('#site-navigation .search-field').hide();
 
-$('.search-submit').click(function(e){ 
+$('#site-navigation .search-submit').click(function(e){ 
     e.stopPropagation();
-    $('.search-field').slideToggle();
+    $('#site-navigation .search-field').slideToggle();
 });
 
-$('.search-field').blur(function(){
-  $('.search-field').css({'display':'none'});
+$('#site-navigation .search-field').blur(function(){
+  $('#site-navigation .search-field').css({'display':'none'});
 });
 
-$('.search-field').click(function(e){
+$('#site-navigation .search-field').click(function(e){
     e.stopPropagation();
 });
 
-$('.search-submit').click(function(){
-  $('.search-field').focus();
+$('#site-navigation .search-submit').click(function(){
+  $('#site-navigation .search-field').focus();
 })
 
-$('.search-field').on('submit',function(event){
+$('#site-navigation .search-field').on('submit',function(event){
   event.preventDefault();
-  window.location.href('?s=' + $('.search-field').val());
+  window.location.href('?s=' + $('#site-navigation .search-field').val());
 });
 
 }(jQuery));
